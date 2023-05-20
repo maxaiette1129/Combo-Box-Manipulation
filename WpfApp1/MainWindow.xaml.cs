adda0bb7fdcd943f6c4143a4b05d24c026cf4fd2
maxaiette1129 <maxaiettec@gmail.com>
2023-05-20 12:17:09 +0800

    new changes

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.ExceptionServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -29,6 +30,38 @@ namespace WpfApp1
         }
 
 
+        public class CostumeNumber
+        {
+            public string countDisplay = "First Number";
+            public int countValue = 10;
+        }
+
+        public void InitCustumCountValue()
+        {
+            List<CostumeNumber> lstCostumeNumber = new List<CostumeNumber>();
+            CostumeNumber Display = new CostumeNumber();
+
+
+            Display.countDisplay = "New Display";
+            Display.countValue = 50;
+
+            lstCostumeNumber.Add(Display);
+
+            CostumeNumber Display2 = new CostumeNumber
+            {
+                countValue = 100,
+                countDisplay = "2nd Display",
+
+            };
+            lstCostumeNumber.Add(Display2 );
+
+            lstCostumeNumber.Add(new CostumeNumber { countDisplay = "3rd Display", countValue = 150 });
+
+            
+           
+        }
+
+
         private void UserCMB()
         {
 
@@ -43,12 +76,47 @@ namespace WpfApp1
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            if (userAuturityList.SelectedIndex == 0) MessageBox.Show("Selecting User authority required!");
-            else
-            { MessageBox.Show($"You have Selected {userAuturityList.Text}" +
-                $"\nLevel of {userAuturityList.SelectedIndex}");
-              
+           var selectedValue = userAuturityList.SelectedItem.ToString();
+            int autorityLevel = 0;
+            int autoritySecurity = 0;
+
+            
+
+            switch (selectedValue) 
+            {
+                case "Guest":
+                    autorityLevel = -1;
+                    autoritySecurity = 0;
+                   
+                    break;
+                case "Student":
+                    autorityLevel = 2 ;
+                    autoritySecurity = 1;
+                   
+                    break;
+                case "Admin":
+                    autorityLevel = 3;
+                    autoritySecurity = 5;
+                 
+                    break;
+                case "Teacher":
+                    autorityLevel = 6;
+                    autoritySecurity = 9;
+                   
+                    break;
+                default :
+                    MessageBox.Show("No Item Selected");
+                    break;
             }
+
+            if(userAuturityList.SelectedIndex >= 1) 
+            {
+                string isSelected = $"The item selected is {selectedValue}" +
+                                           $"\n Autority Level is {autorityLevel}" +
+                                           $"\n and the security level is {autoritySecurity}";
+                MessageBox.Show(isSelected);
+            }
+
         }
     }
 }
